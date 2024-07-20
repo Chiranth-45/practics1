7ee95e06591f090c3c7d90c93a3a6e9dbd7712a5
chiranth <chiranthdv45@gmail.com>
2024-07-20 11:51:51 +0530

    added all files

---
+++ practicsfolder/switch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int n=4;
+   int mid=7/2+1;
+   // for(int i=1;i<=n;i++){
+   //    for(int j=1;j<=n;j++){
+   //     if(i==mid|| j==mid)
+   //       cout<<"*";
+   //       else cout<<" ";
+   //    }
+   //    cout<<endl;
+   // }
+  for(int i=1;i<=4;i++){
+   for(int j=1;j<=4-i;j++){
+      cout<<" "; 
+      }
+     for(int j=1;j<=2*i-1;j++){
+      cout<<"*"; 
+    }
+    cout<<endl;
+  }
+  for(int i=3;i>=1;i--){
+    for(int j=1;j<=4-i;j++){
+      cout<<" "; 
+      }
+     for(int j=1;j<=2*i-1;j++){
+      cout<<"*"; 
+    }
+    cout<<endl;
+  }
+
+  }
